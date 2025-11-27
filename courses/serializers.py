aa495f21d94385bdbf26881ceacaafc8f1ac630d
Keithclinton <keithnyaburi@gmail.com>
2025-11-27 12:54:20 +0300

    serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -16,44 +16,31 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 
+
 # --- Nested Serializers ---
 from .models_resource import Assignment, Quiz, Resource
-
-from rest_framework import serializers
-
-def get_attachment_serializer():
-    from assignments.serializers import AttachmentSerializer
-    return AttachmentSerializer
+from assignments.serializers import AttachmentSerializer
 
 
 class AssignmentSerializer(serializers.ModelSerializer):
-    attachments = serializers.SerializerMethodField()
+    attachments = AttachmentSerializer(many=True, read_only=True)
     class Meta:
         model = Assignment
         fields = ['id', 'title', 'description', 'attachments']
-    def get_attachments(self, obj):
-        AttachmentSerializer = get_attachment_serializer()
-        return AttachmentSerializer(obj.attachments.all(), many=True).data
 
 
 class QuizSerializer(serializers.ModelSerializer):
-    attachments = serializers.SerializerMethodField()
+    attachments = AttachmentSerializer(many=True, read_only=True)
     class Meta:
         model = Quiz
         fields = ['id', 'title', 'description', 'attachments']
-    def get_attachments(self, obj):
-        AttachmentSerializer = get_attachment_serializer()
-        return AttachmentSerializer(obj.attachments.all(), many=True).data
 
 
 class ResourceSerializer(serializers.ModelSerializer):
-    attachments = serializers.SerializerMethodField()
+    attachments = AttachmentSerializer(many=True, read_only=True)
     class Meta:
         model = Resource
         fields = ['id', 'title', 'description', 'attachments']
-    def get_attachments(self, obj):
-        AttachmentSerializer = get_attachment_serializer()
-        return AttachmentSerializer(obj.attachments.all(), many=True).data
 
 class CourseSectionSerializer(serializers.ModelSerializer):
     assignments = AssignmentSerializer(many=True, read_only=True)
