498bc5c2935b3a4804a5cde8e11c24239cdf5832
Keithclinton <keithnyaburi@gmail.com>
2025-11-27 10:31:16 +0300

    Public url for files

--- assignments/serializers.py
+++ assignments/serializers.py
@@ -8,11 +8,22 @@ class AssignmentSerializer(serializers.ModelSerializer):
         fields = ['id', 'course', 'title', 'description', 'due_date']
 
 
+
 class SubmissionSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
     class Meta:
         model = Submission
         fields = ['id', 'assignment', 'user', 'submitted_at', 'file', 'data']
         read_only_fields = ['id', 'submitted_at']
+
+    def get_file(self, obj):
+        if obj.file:
+            try:
+                return obj.file.url
+            except Exception:
+                return None
+        return None
 
 
 class AssignmentGradeSerializer(serializers.ModelSerializer):
