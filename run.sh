fb4c8a3447fdda7ef96762b439ec3dd6d755c31b
Keithclinton <keithnyaburi@gmail.com>
2025-11-27 13:32:11 +0300

    serializer

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
 echo "Starting Gunicorn..."
-exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 eccgd_backend.wsgi:application
+exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 0 eccgd_backend.wsgi:application --log-level info
